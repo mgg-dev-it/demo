a08fbcad847213e1b310b7b5296c1e105c93a2ef
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2021-03-28 17:56:00 +0200

    .

---
+++ SpringBoot/demo-library/src/test/java/mggdevit/demolibrary/DemoLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package mggdevit.demolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
