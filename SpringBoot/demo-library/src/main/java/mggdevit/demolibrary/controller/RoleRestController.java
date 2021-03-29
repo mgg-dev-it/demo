257ac09162a00c7013186aa4a73d722ca96ea2f4
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2021-03-29 14:49:25 +0200

    .

--- SpringBoot/demo-library/src/main/java/mggdevit/demolibrary/controller/RoleRestController.java
+++ SpringBoot/demo-library/src/main/java/mggdevit/demolibrary/controller/RoleRestController.java
@@ -3,8 +3,14 @@ package mggdevit.demolibrary.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +32,22 @@ public class RoleRestController {
 	@GetMapping("/{id}")
 	Optional<Role> geRoleById(@PathVariable Long id) {
 		return roleRepository.findById(id);
+	}
+
+	@PostMapping
+	Role postRole(@RequestBody Role role) {
+		return roleRepository.save(role);
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<Role> putRole(@PathVariable Long id, @RequestBody Role user) {
+		return (!roleRepository.existsById(id)) ? new ResponseEntity<>(roleRepository.save(user), HttpStatus.CREATED)
+				: new ResponseEntity<>(roleRepository.save(user), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	void deleteRole(@PathVariable Long id) {
+		roleRepository.deleteById(id);
 	}
 
 }
--- SpringBoot/demo-library/src/main/java/mggdevit/demolibrary/controller/UserRestController.java
+++ SpringBoot/demo-library/src/main/java/mggdevit/demolibrary/controller/UserRestController.java
@@ -3,8 +3,14 @@ package mggdevit.demolibrary.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +32,22 @@ public class UserRestController {
 	@GetMapping("/{id}")
 	Optional<User> geUserById(@PathVariable Long id) {
 		return userRepository.findById(id);
+	}
+
+	@PostMapping
+	User postUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<User> putUser(@PathVariable Long id, @RequestBody User user) {
+		return (!userRepository.existsById(id)) ? new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED)
+				: new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	void deleteUser(@PathVariable Long id) {
+		userRepository.deleteById(id);
 	}
 
 	/*
