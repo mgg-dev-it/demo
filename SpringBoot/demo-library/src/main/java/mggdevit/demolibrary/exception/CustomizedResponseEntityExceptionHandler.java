55301ea01245ad92d65fccb6e6530cfeeadade9f
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2021-04-04 19:26:03 +0200

    .

--- SpringBoot/demo-library/src/main/java/mggdevit/demolibrary/exception/CustomizedResponseEntityExceptionHandler.java
+++ SpringBoot/demo-library/src/main/java/mggdevit/demolibrary/exception/CustomizedResponseEntityExceptionHandler.java
@@ -8,9 +8,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
+//import javax.validation.ConstraintViolation;
+//import javax.validation.ConstraintViolationException;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -34,14 +33,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 //    }
 
 	// @Validate For Validating Path Variables and Request Parameters
-	@ExceptionHandler(ConstraintViolationException.class)
-	public ResponseEntity<Object> constraintViolationException(ConstraintViolationException ex, WebRequest request)
+	@ExceptionHandler(javax.validation.ConstraintViolationException.class)
+	public ResponseEntity<Object> constraintViolationException(javax.validation.ConstraintViolationException ex, WebRequest request)
 			throws IOException {
-		// System.err.println("adasdasdasdasdasdasdasdas");
+		//System.err.println("constraintViolationException");
 		// response.sendError(HttpStatus.BAD_REQUEST.value());
 
 		List<String> errors = new ArrayList<String>();
-		for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+		for (javax.validation.ConstraintViolation<?> violation : ex.getConstraintViolations()) {
 //            errors.add(violation.getRootBeanClass().getName() + " " + violation.getPropertyPath() + ": " + violation.getMessage());
 			errors.add(violation.getRootBeanClass().getName() + " " + violation.getPropertyPath() + ": "
 					+ violation.getMessage());
@@ -63,23 +62,65 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<>(body, new HttpHeaders(), HttpStatus.BAD_REQUEST);
 	}
 
-	// error handle for @Valid
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
+	@ExceptionHandler(org.hibernate.exception.ConstraintViolationException.class)
+	public ResponseEntity<Object> constraintViolationException(org.hibernate.exception.ConstraintViolationException ex, WebRequest request)
+			throws IOException {
+		//System.err.println("constraintViolationException");
+		// response.sendError(HttpStatus.BAD_REQUEST.value());
+
+		List<String> errors = new ArrayList<String>();
+//		for (org.hibernate.exception.ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+//			errors.add(violation.getRootBeanClass().getName() + " " + violation.getPropertyPath() + ": "
+//					+ violation.getMessage());
+//		}
+		errors.add(ex.getMessage());
+		errors.add(ex.getSQLException().getMessage());
+		
 
 		Map<String, Object> body = new LinkedHashMap<>();
 		body.put("timestamp", new Date());
-		body.put("status", status.value());
+		body.put("status", HttpStatus.BAD_REQUEST);
 
-		// Get all fields errors
-		List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(x -> x.getDefaultMessage())
-				.collect(Collectors.toList());
+//        //Get all fields errors
+//        List<String> errors = ex.getBindingResult()
+//                .getFieldErrors()
+//                .stream()
+//                .map(x -> x.getDefaultMessage())
+//                .collect(Collectors.toList());
 
 		body.put("errors", errors);
 
-		return new ResponseEntity<>(body, headers, status);
-
+		return new ResponseEntity<>(body, new HttpHeaders(), HttpStatus.BAD_REQUEST);
 	}
 
+//	@ExceptionHandler(org.springframework.dao.DataIntegrityViolationException.class)
+//	public ResponseEntity<Object> constraintViolationException(org.springframework.dao.DataIntegrityViolationException ex, WebRequest request)
+//			throws IOException {
+//		//System.err.println("constraintViolationException");
+//		// response.sendError(HttpStatus.BAD_REQUEST.value());
+//		
+//		List<String> errors = new ArrayList<String>();
+////		for (org.hibernate.exception.ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+////			errors.add(violation.getRootBeanClass().getName() + " " + violation.getPropertyPath() + ": "
+////					+ violation.getMessage());
+////		}
+//		errors.add(ex.getMessage());
+//		
+//		
+//		Map<String, Object> body = new LinkedHashMap<>();
+//		body.put("timestamp", new Date());
+//		body.put("status", HttpStatus.BAD_REQUEST);
+//		
+////        //Get all fields errors
+////        List<String> errors = ex.getBindingResult()
+////                .getFieldErrors()
+////                .stream()
+////                .map(x -> x.getDefaultMessage())
+////                .collect(Collectors.toList());
+//		
+//		body.put("errors", errors);
+//		
+//		return new ResponseEntity<>(body, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+//	}
+	
 }
