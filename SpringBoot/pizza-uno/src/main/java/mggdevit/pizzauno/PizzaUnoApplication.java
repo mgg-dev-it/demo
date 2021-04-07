0af3de1865a890ae0ce63199784520e151ea6f89
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2021-04-07 19:40:33 +0200

    .

---
+++ SpringBoot/pizza-uno/src/main/java/mggdevit/pizzauno/PizzaUnoApplication.java
@@ -0,0 +1,13 @@
+package mggdevit.pizzauno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaUnoApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/pizza-uno/src/test/java/mggdevit/pizzauno/PizzaUnoApplicationTests.java
@@ -0,0 +1,13 @@
+package mggdevit.pizzauno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaUnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
