2cbd845884ed4f972d2c3fc05d893810a36724a1
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2021-04-12 10:08:49 +0200

    .

--- SpringBoot/pizza-due/src/main/java/mggdevit/pizzadue/util/Calculator.java
+++ SpringBoot/pizza-due/src/main/java/mggdevit/pizzadue/util/Calculator.java
@@ -39,16 +39,17 @@ public class Calculator {
 			hmPizzaByPrice.put(p.getValue(), p.getName());
 		});
 
-		alResultList.forEach((ArrayList<Integer> al) -> {
+		for (int j = 0; j < 5 && j < alResultList.size(); j++) {
+			ArrayList<Integer> al = alResultList.get(j);
 			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < al.size() - 1; i++) {
 				if (i > 0)
 					sb.append(", ");
 				sb.append(hmPizzaByPrice.get(al.get(i)));
 			}
-			if (pll.size() < 5)
-				pll.add(new PizzaList(sb.toString(), al.get(al.size() - 1)));
-		});
+			pll.add(new PizzaList(sb.toString(), al.get(al.size() - 1)));
+		}
+
 		return pll;
 	}
 }
--- SpringBoot/pizza-uno/src/main/java/mggdevit/pizzauno/util/Calculator.java
+++ SpringBoot/pizza-uno/src/main/java/mggdevit/pizzauno/util/Calculator.java
@@ -38,18 +38,17 @@ public class Calculator {
 			hmPizzaByPrice.put(p.getValue(), p.getName());
 		});
 
-		alResultList.forEach((ArrayList<Integer> al) -> {
-			if (pll.size() < 5) {
-				StringBuilder sb = new StringBuilder();
-				for (int i = 0; i < al.size() - 1; i++) {
-					if (i > 0)
-						sb.append(", ");
-					sb.append(hmPizzaByPrice.get(al.get(i)));
-				}
-				pll.add(new PizzaList(sb.toString(), al.get(al.size() - 1)));
+		for (int j = 0; j < 5 && j < alResultList.size(); j++) {
+			ArrayList<Integer> al = alResultList.get(j);
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < al.size() - 1; i++) {
+				if (i > 0)
+					sb.append(", ");
+				sb.append(hmPizzaByPrice.get(al.get(i)));
 			}
-		});
-		
+			pll.add(new PizzaList(sb.toString(), al.get(al.size() - 1)));
+		}
+
 		return pll;
 	}
 }
